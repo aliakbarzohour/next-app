e09aa149d149a15843eb2a16cc37767ca414011c
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-04-08 19:41:38 +0430

    change navbar styling

--- components/Nav/nav.jsx
+++ components/Nav/nav.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import Link from "next/link";
+import styled from "styled-components"
+import Link from "next/link"
 
 const Navbar = styled.nav`
   position: absolute;
